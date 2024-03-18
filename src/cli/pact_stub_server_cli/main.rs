c23d2468c9e374345df1d80672248a60f4bf0e85
Yousaf Nabi <you@saf.dev>
2024-03-18 20:35:22 +0000

    chore: cargo fmt

--- src/cli/pact_stub_server_cli/main.rs
+++ src/cli/pact_stub_server_cli/main.rs
@@ -88,180 +88,239 @@ use std::env;
 use std::process::ExitCode;
 use std::str::FromStr;
 
-use clap::{Command, Arg, ArgMatches, ArgAction, command, crate_version};
 use clap::error::ErrorKind;
+use clap::{command, crate_version, Arg, ArgAction, ArgMatches, Command};
+use loading::load_pacts;
 use pact_models::prelude::*;
 use regex::Regex;
 use tracing::{debug, error, info, warn};
 use tracing_core::LevelFilter;
 use tracing_subscriber::FmtSubscriber;
-use loading::load_pacts;
 
 use server::ServerHandler;
 
+mod loading;
 mod pact_support;
 mod server;
-mod loading;
 
 #[tokio::main]
 async fn main() -> Result<(), ExitCode> {
-  let args: Vec<String> = env::args().collect();
-  handle_command_args(args).await
+    let args: Vec<String> = env::args().collect();
+    handle_command_args(args).await
 }
 
-fn print_version() {
+pub fn print_version() {
     println!("pact stub server version  : v{}", env!("CARGO_PKG_VERSION"));
-    println!("pact specification version: v{}", PactSpecification::V4.version_str());
+    println!(
+        "pact specification version: v{}",
+        PactSpecification::V4.version_str()
+    );
 }
 
 fn integer_value(v: &str) -> Result<u16, String> {
-    v.parse::<u16>().map_err(|e| format!("'{}' is not a valid port value: {}", v, e) )
+    v.parse::<u16>()
+        .map_err(|e| format!("'{}' is not a valid port value: {}", v, e))
 }
 
 fn regex_value(v: &str) -> Result<Regex, String> {
-  if v.is_empty() {
-    Err("Regular expression is empty".to_string())
-  } else {
-    Regex::new(v).map_err(|e| format!("'{}' is not a valid regular expression: {}", v, e))
-  }
+    if v.is_empty() {
+        Err("Regular expression is empty".to_string())
+    } else {
+        Regex::new(v).map_err(|e| format!("'{}' is not a valid regular expression: {}", v, e))
+    }
 }
 
 /// Source for loading pacts
 #[derive(Debug, Clone)]
 pub enum PactSource {
-  /// Load the pact from a pact file
-  File(String),
-  /// Load all the pacts from a Directory
-  Dir(String),
-  /// Load the pact from a URL
-  URL(String, Option<HttpAuth>),
-  /// Load all pacts from a Pact Broker
-  Broker {
-    /// Broker URL
-    url: String,
-    /// Any required auth
-    auth: Option<HttpAuth>,
-    /// Consumer names to filter Pacts with
-    consumers: Vec<Regex>,
-    /// Provider names to filter Pacts with
-    providers: Vec<Regex>
-  },
-  /// Source that is not known, only used for unit testing
-  Unknown
+    /// Load the pact from a pact file
+    File(String),
+    /// Load all the pacts from a Directory
+    Dir(String),
+    /// Load the pact from a URL
+    URL(String, Option<HttpAuth>),
+    /// Load all pacts from a Pact Broker
+    Broker {
+        /// Broker URL
+        url: String,
+        /// Any required auth
+        auth: Option<HttpAuth>,
+        /// Consumer names to filter Pacts with
+        consumers: Vec<Regex>,
+        /// Provider names to filter Pacts with
+        providers: Vec<Regex>,
+    },
+    /// Source that is not known, only used for unit testing
+    Unknown,
 }
 
 fn pact_source(matches: &ArgMatches) -> Vec<PactSource> {
-  let mut sources = vec![];
+    let mut sources = vec![];
 
-  if let Some(values) = matches.get_many::<String>("file") {
-    sources.extend(values.map(|v| PactSource::File(v.clone())).collect::<Vec<PactSource>>());
-  }
+    if let Some(values) = matches.get_many::<String>("file") {
+        sources.extend(
+            values
+                .map(|v| PactSource::File(v.clone()))
+                .collect::<Vec<PactSource>>(),
+        );
+    }
 
-  if let Some(values) = matches.get_many::<String>("dir") {
-    sources.extend(values.map(|v| PactSource::Dir(v.clone())).collect::<Vec<PactSource>>());
-  }
+    if let Some(values) = matches.get_many::<String>("dir") {
+        sources.extend(
+            values
+                .map(|v| PactSource::Dir(v.clone()))
+                .collect::<Vec<PactSource>>(),
+        );
+    }
 
-  if let Some(values) = matches.get_many::<String>("url") {
-    sources.extend(values.map(|v| {
-      let auth = matches.get_one::<String>("user")
-        .map(|u| {
-          let mut auth = u.split(':');
-          HttpAuth::User(auth.next().unwrap().to_string(), auth.next().map(|p| p.to_string()))
-        })
-        .or_else(|| matches.get_one::<String>("token").map(|v| HttpAuth::Token(v.clone())));
-      PactSource::URL(v.clone(), auth)
-    }).collect::<Vec<PactSource>>());
-  }
+    if let Some(values) = matches.get_many::<String>("url") {
+        sources.extend(
+            values
+                .map(|v| {
+                    let auth = matches
+                        .get_one::<String>("user")
+                        .map(|u| {
+                            let mut auth = u.split(':');
+                            HttpAuth::User(
+                                auth.next().unwrap().to_string(),
+                                auth.next().map(|p| p.to_string()),
+                            )
+                        })
+                        .or_else(|| {
+                            matches
+                                .get_one::<String>("token")
+                                .map(|v| HttpAuth::Token(v.clone()))
+                        });
+                    PactSource::URL(v.clone(), auth)
+                })
+                .collect::<Vec<PactSource>>(),
+        );
+    }
 
-  if let Some(url) = matches.get_one::<String>("broker-url") {
-    let auth = matches.get_one::<String>("user")
-      .map(|u| {
-        let mut auth = u.split(':');
-        HttpAuth::User(auth.next().unwrap().to_string(), auth.next().map(|p| p.to_string()))
-      })
-      .or_else(|| matches.get_one::<String>("token").map(|v| HttpAuth::Token(v.clone())));
-    debug!("Loading all pacts from Pact Broker at {} using {} authentication", url,
-      auth.clone().map(|auth| auth.to_string()).unwrap_or_else(|| "no".to_string()));
-    sources.push(PactSource::Broker {
-      url: url.to_string(),
-      auth,
-      consumers: matches.get_many::<Regex>("consumer-name").unwrap_or_default().into_iter().cloned().collect(),
-      providers: matches.get_many::<Regex>("provider-name").unwrap_or_default().into_iter().cloned().collect()
-    });
-  }
+    if let Some(url) = matches.get_one::<String>("broker-url") {
+        let auth = matches
+            .get_one::<String>("user")
+            .map(|u| {
+                let mut auth = u.split(':');
+                HttpAuth::User(
+                    auth.next().unwrap().to_string(),
+                    auth.next().map(|p| p.to_string()),
+                )
+            })
+            .or_else(|| {
+                matches
+                    .get_one::<String>("token")
+                    .map(|v| HttpAuth::Token(v.clone()))
+            });
+        debug!(
+            "Loading all pacts from Pact Broker at {} using {} authentication",
+            url,
+            auth.clone()
+                .map(|auth| auth.to_string())
+                .unwrap_or_else(|| "no".to_string())
+        );
+        sources.push(PactSource::Broker {
+            url: url.to_string(),
+            auth,
+            consumers: matches
+                .get_many::<Regex>("consumer-name")
+                .unwrap_or_default()
+                .into_iter()
+                .cloned()
+                .collect(),
+            providers: matches
+                .get_many::<Regex>("provider-name")
+                .unwrap_or_default()
+                .into_iter()
+                .cloned()
+                .collect(),
+        });
+    }
 
-  sources
+    sources
 }
 
 async fn handle_command_args(args: Vec<String>) -> Result<(), ExitCode> {
-  let app = build_args();
-  match app.try_get_matches_from(args) {
-    Ok(ref matches) => {
-      let level = matches.get_one::<String>("loglevel").cloned()
-        .unwrap_or_else(|| "info".to_string());
-      setup_logger(level.as_str());
-      let sources = pact_source(matches);
+    let app = build_args();
+    match app.try_get_matches_from(args) {
+        Ok(results) => handle_matches(&results).await,
 
-      let pacts = load_pacts(sources, matches.get_flag("insecure-tls"),
-        matches.get_one("ext")).await;
-      if pacts.iter().any(|p| p.is_err()) {
+        Err(ref err) => match err.kind() {
+            ErrorKind::DisplayHelp => {
+                println!("{}", err);
+                Ok(())
+            }
+            ErrorKind::DisplayVersion => {
+                print_version();
+                println!();
+                Ok(())
+            }
+            _ => err.exit(),
+        },
+    }
+}
+
+pub async fn handle_matches(matches: &ArgMatches) -> Result<(), ExitCode> {
+    let level = matches
+        .get_one::<String>("loglevel")
+        .cloned()
+        .unwrap_or_else(|| "info".to_string());
+    setup_logger(level.as_str());
+    let sources = pact_source(matches);
+
+    let pacts = load_pacts(
+        sources,
+        matches.get_flag("insecure-tls"),
+        matches.get_one("ext"),
+    )
+    .await;
+    if pacts.iter().any(|p| p.is_err()) {
         error!("There were errors loading the pact files.");
-        for error in pacts.iter()
-          .filter(|p| p.is_err())
-          .map(|e| match e {
+        for error in pacts.iter().filter(|p| p.is_err()).map(|e| match e {
             Err(err) => err.clone(),
-            _ => panic!("Internal Code Error - was expecting an error but was not")
-          }) {
-          error!("  - {}", error);
+            _ => panic!("Internal Code Error - was expecting an error but was not"),
+        }) {
+            error!("  - {}", error);
         }
         Err(ExitCode::from(3))
-      } else {
+    } else {
         let port = *matches.get_one::<u16>("port").unwrap_or(&0);
         let provider_state = matches.get_one::<Regex>("provider-state").cloned();
-        let provider_state_header_name = matches.get_one::<String>("provider-state-header-name").cloned();
+        let provider_state_header_name = matches
+            .get_one::<String>("provider-state-header-name")
+            .cloned();
         let empty_provider_states = matches.get_flag("empty-provider-state");
-        let pacts = pacts.iter()
-          .map(|result| {
-            // Currently, as_v4_pact won't fail as it upgrades older formats to V4, so is safe to unwrap
-            let (p, s) = result.as_ref().unwrap();
-            (p.as_v4_pact().unwrap(), s.clone())
-          })
-          .collect::<Vec<_>>();
+        let pacts = pacts
+            .iter()
+            .map(|result| {
+                // Currently, as_v4_pact won't fail as it upgrades older formats to V4, so is safe to unwrap
+                let (p, s) = result.as_ref().unwrap();
+                (p.as_v4_pact().unwrap(), s.clone())
+            })
+            .collect::<Vec<_>>();
         let interactions: usize = pacts.iter().map(|(p, _)| p.interactions.len()).sum();
-        info!("Loaded {} pacts ({} total interactions)", pacts.len(), interactions);
+        info!(
+            "Loaded {} pacts ({} total interactions)",
+            pacts.len(),
+            interactions
+        );
         let auto_cors = matches.get_flag("cors");
         let referer = matches.get_flag("cors-referer");
         let server_handler = ServerHandler::new(
-          pacts,
-          auto_cors,
-          referer,
-          provider_state,
-          provider_state_header_name,
-          empty_provider_states);
-        tokio::task::spawn_blocking(move || {
-          server_handler.start_server(port)
-        }).await.unwrap()
-      }
-    },
-    Err(ref err) => {
-      match err.kind() {
-        ErrorKind::DisplayHelp => {
-          println!("{}", err);
-          Ok(())
-        },
-        ErrorKind::DisplayVersion => {
-          print_version();
-          println!();
-          Ok(())
-        },
-        _ => err.exit()
-      }
+            pacts,
+            auto_cors,
+            referer,
+            provider_state,
+            provider_state_header_name,
+            empty_provider_states,
+        );
+        tokio::task::spawn_blocking(move || server_handler.start_server(port))
+            .await
+            .unwrap()
     }
-  }
 }
-
 pub fn build_args() -> Command {
-  Command::new("stub")
+    Command::new("stub")
     .about(format!("Pact Stub Server {}", crate_version!()))
     .arg_required_else_help(true)
     .disable_version_flag(true)
@@ -373,18 +432,18 @@ pub fn build_args() -> Command {
 }
 
 fn setup_logger(level: &str) {
-  let log_level = match level {
-    "none" => LevelFilter::OFF,
-    _ => LevelFilter::from_str(level).unwrap_or(LevelFilter::INFO)
-  };
-  let subscriber = FmtSubscriber::builder()
-    .compact()
-    .with_max_level(log_level)
-    .with_thread_names(true)
-    .finish();
-  if let Err(err) = tracing::subscriber::set_global_default(subscriber) {
-    eprintln!("ERROR: Failed to initialise global tracing subscriber - {err}");
-  };
+    let log_level = match level {
+        "none" => LevelFilter::OFF,
+        _ => LevelFilter::from_str(level).unwrap_or(LevelFilter::INFO),
+    };
+    let subscriber = FmtSubscriber::builder()
+        .compact()
+        .with_max_level(log_level)
+        .with_thread_names(true)
+        .finish();
+    if let Err(err) = tracing::subscriber::set_global_default(subscriber) {
+        eprintln!("ERROR: Failed to initialise global tracing subscriber - {err}");
+    };
 }
 
 // #[cfg(test)]
--- src/pact_plugin_cli/main/list.rs
+++ src/pact_plugin_cli/main/list.rs
@@ -14,126 +14,144 @@ use tracing::trace;
 use pact_plugin_driver::plugin_models::PactPluginManifest;
 use pact_plugin_driver::repository::fetch_repository_index;
 
-use super::resolve_plugin_dir;
 use super::repository::{APP_USER_AGENT, DEFAULT_INDEX};
+use super::resolve_plugin_dir;
 
 pub fn list_plugins(command: &ArgMatches) -> anyhow::Result<()> {
-  match command.subcommand() {
-    Some(("installed", _)) => list_installed_plugins(),
-    Some(("known", args)) => list_known_plugins(args.get_one::<bool>("show_all_versions").unwrap()),
-    Some((&_, _)) => Err(anyhow!("unrecognized subcommand is shown")),
-    None => Err(anyhow!("help is shown by default")), 
-  }
+    match command.subcommand() {
+        Some(("installed", _)) => list_installed_plugins(),
+        Some(("known", args)) => list_known_plugins(&args.get_flag("show_all_versions")),
+        Some((&_, _)) => Err(anyhow!("unrecognized subcommand is shown")),
+        None => Err(anyhow!("help is shown by default")),
+    }
 }
 
 fn list_known_plugins(show_all_versions: &bool) -> anyhow::Result<()> {
-  let runtime = tokio::runtime::Builder::new_multi_thread()
-    .enable_all()
-    .build()?;
-  let http_client = reqwest::ClientBuilder::new()
-    .user_agent(APP_USER_AGENT)
-    .build()?;
-  let index = runtime.block_on(fetch_repository_index(&http_client, Some(DEFAULT_INDEX)))?;
-  trace!("Result = {:?}", index);
-  runtime.shutdown_background();
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?;
+    let http_client = reqwest::ClientBuilder::new()
+        .user_agent(APP_USER_AGENT)
+        .build()?;
+    let index = runtime.block_on(fetch_repository_index(&http_client, Some(DEFAULT_INDEX)))?;
+    trace!("Result = {:?}", index);
+    runtime.shutdown_background();
 
-  let mut table = Table::new();
-  if *show_all_versions {
-    table
-      .load_preset(UTF8_FULL)
-      .set_header(vec!["Name", "Version", "Source", "Value"]);
+    let mut table = Table::new();
+    if *show_all_versions {
+        table
+            .load_preset(UTF8_FULL)
+            .set_header(vec!["Name", "Version", "Source", "Value"]);
 
-    for entry in index.entries.values() {
-      for version in &entry.versions {
-        table.add_row(vec![
-          entry.name.as_str(),
-          version.version.as_str(),
-          version.source.name().as_str(),
-          version.source.value().as_str()
-        ]);
-      }
+        for entry in index.entries.values() {
+            for version in &entry.versions {
+                table.add_row(vec![
+                    entry.name.as_str(),
+                    version.version.as_str(),
+                    version.source.name().as_str(),
+                    version.source.value().as_str(),
+                ]);
+            }
+        }
+    } else {
+        table
+            .load_preset(UTF8_FULL)
+            .set_header(vec!["Name", "Latest Version", "Num Versions"]);
+
+        for entry in index.entries.values() {
+            table.add_row(vec![
+                entry.name.as_str(),
+                entry.latest_version.as_str(),
+                entry.versions.len().to_string().as_str(),
+            ]);
+        }
     }
-  } else {
-    table
-      .load_preset(UTF8_FULL)
-      .set_header(vec!["Name", "Latest Version", "Num Versions"]);
 
-    for entry in index.entries.values() {
-      table.add_row(vec![
-        entry.name.as_str(),
-        entry.latest_version.as_str(),
-        entry.versions.len().to_string().as_str()
-      ]);
-    }
-  }
+    println!("{table}");
 
-  println!("{table}");
-
-  Ok(())
+    Ok(())
 }
 
 fn list_installed_plugins() -> anyhow::Result<()> {
-  let mut table = Table::new();
-  table
-    .load_preset(UTF8_FULL)
-    .set_header(vec!["Name", "Version", "Interface Version", "Directory", "Status"]);
-
-  for (manifest, _, status) in plugin_list()?.iter().sorted_by(manifest_sort_fn) {
-    table.add_row(vec![
-      manifest.name.as_str(),
-      manifest.version.as_str(),
-      manifest.plugin_interface_version.to_string().as_str(),
-      manifest.plugin_dir.to_string().as_str(),
-      if *status { "enabled" } else { "disabled" }
+    let mut table = Table::new();
+    table.load_preset(UTF8_FULL).set_header(vec![
+        "Name",
+        "Version",
+        "Interface Version",
+        "Directory",
+        "Status",
     ]);
-  }
 
-  println!("{table}");
+    for (manifest, _, status) in plugin_list()?.iter().sorted_by(manifest_sort_fn) {
+        table.add_row(vec![
+            manifest.name.as_str(),
+            manifest.version.as_str(),
+            manifest.plugin_interface_version.to_string().as_str(),
+            manifest.plugin_dir.to_string().as_str(),
+            if *status { "enabled" } else { "disabled" },
+        ]);
+    }
 
-  Ok(())
+    println!("{table}");
+
+    Ok(())
 }
 
 pub fn plugin_list() -> anyhow::Result<Vec<(PactPluginManifest, PathBuf, bool)>> {
-  let (_, plugin_dir) = resolve_plugin_dir();
-  let dir = PathBuf::from(plugin_dir);
-  if dir.exists() {
-    let mut plugins = vec![];
-    for entry in fs::read_dir(dir)? {
-      let path = entry?.path();
-      if path.is_dir() {
-        let manifest_file = path.join("pact-plugin.json");
-        if manifest_file.exists() && manifest_file.is_file() {
-          let file = File::open(manifest_file.clone())?;
-          let reader = BufReader::new(file);
-          let manifest: PactPluginManifest = serde_json::from_reader(reader)?;
-          plugins.push((PactPluginManifest {
-            plugin_dir: path.display().to_string(),
-            ..manifest
-          }, manifest_file.clone(), true));
-        } else {
-          let manifest_file = path.join("pact-plugin.json.disabled");
-          if manifest_file.exists() && manifest_file.is_file() {
-            let file = File::open(manifest_file.clone())?;
-            let reader = BufReader::new(file);
-            let manifest: PactPluginManifest = serde_json::from_reader(reader)?;
-            plugins.push((PactPluginManifest {
-              plugin_dir: path.display().to_string(),
-              ..manifest
-            }, manifest_file.clone(), false));
-          }
+    let (_, plugin_dir) = resolve_plugin_dir();
+    let dir = PathBuf::from(plugin_dir);
+    if dir.exists() {
+        let mut plugins = vec![];
+        for entry in fs::read_dir(dir)? {
+            let path = entry?.path();
+            if path.is_dir() {
+                let manifest_file = path.join("pact-plugin.json");
+                if manifest_file.exists() && manifest_file.is_file() {
+                    let file = File::open(manifest_file.clone())?;
+                    let reader = BufReader::new(file);
+                    let manifest: PactPluginManifest = serde_json::from_reader(reader)?;
+                    plugins.push((
+                        PactPluginManifest {
+                            plugin_dir: path.display().to_string(),
+                            ..manifest
+                        },
+                        manifest_file.clone(),
+                        true,
+                    ));
+                } else {
+                    let manifest_file = path.join("pact-plugin.json.disabled");
+                    if manifest_file.exists() && manifest_file.is_file() {
+                        let file = File::open(manifest_file.clone())?;
+                        let reader = BufReader::new(file);
+                        let manifest: PactPluginManifest = serde_json::from_reader(reader)?;
+                        plugins.push((
+                            PactPluginManifest {
+                                plugin_dir: path.display().to_string(),
+                                ..manifest
+                            },
+                            manifest_file.clone(),
+                            false,
+                        ));
+                    }
+                }
+            }
         }
-      }
+        Ok(plugins)
+    } else {
+        Err(anyhow!(
+            "Plugin directory '{}' does not exist!",
+            dir.display()
+        ))
     }
-    Ok(plugins)
-  } else {
-    Err(anyhow!("Plugin directory '{}' does not exist!", dir.display()))
-  }
 }
 
-fn manifest_sort_fn(a: &&(PactPluginManifest, PathBuf, bool), b: &&(PactPluginManifest, PathBuf, bool)) -> Ordering {
-  if a.0.name == b.0.name {
-    a.0.version.cmp(&b.0.version)
-  } else {
-    a.0.name.cmp(&b.0.name)
-  }
+fn manifest_sort_fn(
+    a: &&(PactPluginManifest, PathBuf, bool),
+    b: &&(PactPluginManifest, PathBuf, bool),
+) -> Ordering {
+    if a.0.name == b.0.name {
+        a.0.version.cmp(&b.0.version)
+    } else {
+        a.0.name.cmp(&b.0.name)
+    }
 }
